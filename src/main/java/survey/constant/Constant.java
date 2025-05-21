d98e9e65f3540d528993e160d8f44aa143746d21
TungDN <tung.daongoc94@gmail.com>
2025-05-21 22:59:08 +0700

    Update title to final

--- src/main/java/survey/constant/Constant.java
+++ src/main/java/survey/constant/Constant.java
@@ -1,8 +1,10 @@
 package survey.constant;
 
 public class Constant {
-    public static String WARNING_TITLE = "Cảnh báo";
-    public static String INFO_TITLE = "Thông báo";
+    private Constant() {}
+
+    public static final String WARNING_TITLE = "Cảnh báo";
+    public static final String INFO_TITLE = "Thông báo";
     public static final String FONT_TYPE = "Times New Roman";
 
 }
